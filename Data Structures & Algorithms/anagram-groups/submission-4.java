e64d63b355d937595d747b691d87bbeaac89eb57
Shobhit Aggarawal <97778494+Shobhit0109@users.noreply.github.com>
2026-06-07 23:48:14 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<String>> groupAnagrams(final String[] strs) {
+        final var map = new HashMap<String, List<String>>(strs.length);
+        for (final var str : strs) {
+            final var hash = getHash(str);
+            final var arr = map.computeIfAbsent(hash, k -> new ArrayList<>());
+            arr.add(str);
+        }
+        return map.values().stream().toList();
+    }
+
+    private String getHash(final String str) {
+        final var arr = new int[26];
+        for (final var ch : str.toCharArray()) {
+            arr[ch - 'a']++;
+        }
+        final var sb = new StringBuilder(64);
+        for (int count : arr) {
+            sb.append('#').append(count);
+        }
+        return sb.toString();
+    }
+}
